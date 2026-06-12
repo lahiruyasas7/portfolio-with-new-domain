f2aa5ed147bf2f6bb5cbd1713ebb5fd303b76fcc
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-06-12 17:17:56 +0530

    feat: change the images to optimized images to fashion nest project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -28,11 +28,11 @@ export const projects: Project[] = [
     description:
       "Developed a full-stack T-shirt design and order management platform for a clothing company, featuring an admin panel for managing products, designers, and order workflows, and a designer portal for creating and uploading T-shirt designs. Built using Next.js, Tailwind CSS, Redux-Saga, and ApexCharts on the frontend, with NestJS, Redis, and BullMQ powering backend operations, and deployed on AWS S3 and EC2 for scalable storage and hosting.",
     images: [
-      "custom-images/optimized_design-1.webp",
-      "custom-images/optimized_admin-1.webp",
-      "custom-images/optimized_admin-2.webp",
-      "custom-images/fnlk-admin-2.PNG",
-      "custom-images/fnlk-admin-4.PNG",
+      "custom-images/optimized_design-1.jpg",
+      "custom-images/optimized_admin-1.jpg",
+      "custom-images/optimized_admin-2.jpg",
+      "custom-images/fnlk-admin-2.jpg",
+      "custom-images/fnlk-admin-4.jpg",
     ],
   },
   {
